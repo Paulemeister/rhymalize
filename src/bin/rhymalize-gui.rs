24f311f2fbb9fa4d6d8b325142e29d09c57f36d0
Paulemeister <annanas6800i@gmail.com>
2024-03-07 23:17:50 +0100

    ported to Application, started on calculating rhymes

--- src/bin/rhymalize-gui.rs
+++ src/bin/rhymalize-gui.rs
@@ -2,10 +2,11 @@ use std::path::Path;
 use std::{fs, vec};
 
 use iced::widget::{column, row, scrollable::Scrollable, Button, Column, Container, Row, Text};
-use iced::Color;
+use iced::{executor, Application, Color};
 use iced::{Background, Settings};
 use iced::{Renderer, Sandbox, Theme};
 
+use iced::command::Command;
 use rhymalize::ipa_utils::fetching::json::JsonLookupConverter;
 use rhymalize::ipa_utils::fetching::IpaConverter;
 use rhymalize::ipa_utils::{self, ipa::*};
@@ -24,61 +25,88 @@ struct App {
     text: Vec<Vec<DisplayWord>>,
 }
 
-impl Sandbox for App {
-    type Message = ();
-
-    fn new() -> Self {
-        let a = "\u{006C}\u{02E0}";
-        let converter = JsonLookupConverter::new(Path::new("./en_US.json")).unwrap();
-        App {
-            //text: fs::read_to_string("./text.txt")
-            //   .unwrap()
-            text: fs::read_to_string("./text.txt")
-                .unwrap()
-                .split("\n")
-                .map(|line| {
-                    line.split(" ")
-                        .map(|word| {
-                            let word2 = word
-                                .to_ascii_lowercase()
-                                .trim()
-                                .replace(",", "")
-                                .replace(".", "");
-                            let ipas2 = converter.get_ipa_single(&word2);
-                            if ipas2.is_err() {
-                                println!("{ipas2:?}")
-                            }
-                            DisplayWord {
-                                text: word.to_string(),
-                                syllables: if let Ok(ipas) = ipas2 {
-                                    Some(
-                                        syls_from_word(
-                                            &ipas[0], // use first possible pronunciation
-                                            &ipa_utils::ipa::english::EnglishSyllableRule,
-                                        )
-                                        .iter()
-                                        .map(|z| {
-                                            (z.to_owned(), Some(Color::from_rgb(1.0, 0.0, 0.0)))
-                                        })
-                                        .collect(),
-                                    )
-                                } else {
-                                    None
-                                },
-                            }
-                        })
-                        .collect()
-                })
-                .collect(),
+impl App {
+    fn calc_rhyme(&mut self) {
+        for a in &mut self.text {
+            for b in a {
+                for c in &mut b.syllables {
+                    for (_, col) in c {
+                        *col = Some(Color::from_rgb(1.0, 0.0, 0.0));
+                    }
+                }
+            }
         }
+    }
+}
+
+#[derive(Debug)]
+enum Message {
+    CalculateRhyme,
+}
+
+impl Application for App {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+    type Theme = Theme;
+
+    fn new(flags: Self::Flags) -> (Self, Command<Message>) {
+        let converter = JsonLookupConverter::new(Path::new("./en_US.json")).unwrap();
+        (
+            App {
+                //text: fs::read_to_string("./text.txt")
+                //   .unwrap()
+                text: fs::read_to_string("./text.txt")
+                    .unwrap()
+                    .split("\n")
+                    .map(|line| {
+                        line.split(" ")
+                            .map(|word| {
+                                let word2 = word
+                                    .to_ascii_lowercase()
+                                    .trim()
+                                    .replace(",", "")
+                                    .replace(".", "");
+                                let ipas2 = converter.get_ipa_single(&word2);
+                                if ipas2.is_err() {
+                                    println!("{ipas2:?}")
+                                }
+                                DisplayWord {
+                                    text: word.to_string(),
+                                    syllables: if let Ok(ipas) = ipas2 {
+                                        Some(
+                                            syls_from_word(
+                                                &ipas[0], // use first possible pronunciation
+                                                &ipa_utils::ipa::english::EnglishSyllableRule,
+                                            )
+                                            .iter()
+                                            .map(|z| {
+                                                (z.to_owned(), None) //Some(Color::from_rgb(1.0, 0.0, 0.0)))
+                                            })
+                                            .collect(),
+                                        )
+                                    } else {
+                                        None
+                                    },
+                                }
+                            })
+                            .collect()
+                    })
+                    .collect(),
+            },
+            Command::perform(async {}, |_| Message::CalculateRhyme),
+        )
     }
 
     fn title(&self) -> String {
         String::from("Rhymalize")
     }
 
-    fn update(&mut self, message: Self::Message) {
-        todo!()
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::CalculateRhyme => self.calc_rhyme(),
+        }
+        Command::none()
     }
 
     // if let Some(syls) = x.syllables {
@@ -107,10 +135,9 @@ impl Sandbox for App {
                             syllables
                                 .iter()
                                 .fold(row!().spacing(5), |srow, (syl, col)| {
-                                    srow.push(
-                                        Text::new(syl.to_string().clone())
-                                            .style(col.unwrap_or_default()),
-                                    )
+                                    srow.push(Text::new(syl.to_string().clone()).style(
+                                        col.unwrap_or_else(|| Color::from_rgb(0.9, 0.9, 0.9)),
+                                    ))
                                 })
                         } else {
                             row!()
